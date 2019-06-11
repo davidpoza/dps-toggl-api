ba5efdf10aa3fecad0cf46cdbc54439756f047d5
David Poza Suarez <pozasuarez@gmail.com>
2019-06-11 13:56:28 +0200

    added PUT /tasks/:id endpoint

--- controllers/task.js
+++ controllers/task.js
@@ -44,6 +44,36 @@ let controller = {
                 res.json({data: {message:"Task deleted succesfully."}});
             })
             .catch(err=>next(err));
+    },
+
+    updateTask: (req, res, next) => {
+        if(!req.params.id)
+            next(new error_types.Error400("id param with task id is rquired."));
+        let update = {};
+        if(req.body.desc) update["desc"] = req.body.desc;
+        if(req.body.date) update["date"] = req.body.date;
+        if(req.body.start_hour) update["start_hour"] = req.body.start_hour;
+        if(req.body.end_hour) update["end_hour"] = req.body.end_hour;
+        if(req.body.project) update["project"] = req.body.project;
+        if(req.body.tags) update["tags"] = req.body.tags;
+
+        Task.findById(req.params.id)
+            .then(data=>{
+                if(!data)
+                    throw(new error_types.Error404("Task not found"));
+                else if (!data.user.equals(req.user._id) && req.user.admin==false){
+                    throw(new error_types.Error403("You are not allowed to modify this task"));
+                }
+                else
+                    return Promise.resolve();
+            })
+            .then(()=>{
+                return Task.findByIdAndUpdate(req.params.id, update, {new:true});
+            })
+            .then((data)=>{
+                res.json({data: data});
+            })
+            .catch(err=>next(err));
     }
 };
 
