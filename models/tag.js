91fa36a260ecbad1772b0c664a815e73ebdce086
David Poza Suarez <pozasuarez@gmail.com>
2019-06-11 17:10:41 +0200

    fix: task field in Tag model has been renamed to tasks

--- models/tag.js
+++ models/tag.js
@@ -4,9 +4,9 @@ const mongoose    = require("mongoose");
 const Schema      = mongoose.Schema;
 
 const TagSchema = Schema({
-    name      : {type: String, required: true},
-    user      : { type: Schema.ObjectId, ref:"User", required: true},
-    task      : [{ type: Schema.ObjectId, ref:"Task"}]
+    name : {type: String, required: true},
+    user : { type: Schema.ObjectId, ref:"User", required: true},
+    tasks: [{ type: Schema.ObjectId, ref:"Task"}]
 });
 
 module.exports = mongoose.model("Tag", TagSchema, "tags");
