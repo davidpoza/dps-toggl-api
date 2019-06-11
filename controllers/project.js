d76abbde5ed9b7ac3acfbf107ef531555be4df69
David Poza Suarez <pozasuarez@gmail.com>
2019-06-11 17:59:16 +0200

    clean: updateTask function, not corresponding

--- controllers/project.js
+++ controllers/project.js
@@ -54,35 +54,6 @@ let controller = {
             .catch(err=>next(err));
     },
 
-    updateTask: (req, res, next) => {
-        if(!req.params.id)
-            next(new error_types.Error400("id param with task id is rquired."));
-        let update = {};
-        if(req.body.desc) update["desc"] = req.body.desc;
-        if(req.body.date) update["date"] = req.body.date;
-        if(req.body.start_hour) update["start_hour"] = req.body.start_hour;
-        if(req.body.end_hour) update["end_hour"] = req.body.end_hour;
-        if(req.body.project) update["project"] = req.body.project;
-        if(req.body.tags) update["tags"] = req.body.tags;
-
-        Task.findById(req.params.id)
-            .then(data=>{
-                if(!data)
-                    throw(new error_types.Error404("Task not found"));
-                else if (!data.user.equals(req.user._id) && req.user.admin==false){
-                    throw(new error_types.Error403("You are not allowed to modify this task"));
-                }
-                else
-                    return Promise.resolve();
-            })
-            .then(()=>{
-                return Task.findByIdAndUpdate(req.params.id, update, {new:true});
-            })
-            .then((data)=>{
-                res.json({data: data});
-            })
-            .catch(err=>next(err));
-    },
     /**
      * only can be fetched the owned projects. Unless you are admin and can fetch any project
      * Parameters via query:
