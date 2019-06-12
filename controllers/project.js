522a6251a00ee4a18b71e1ddcf42d2f0e68b76ba
David Poza Suarez <pozasuarez@gmail.com>
2019-06-12 14:34:25 +0200

    updateProject: it checks if users added/deleted exist or if they already are project members

--- controllers/project.js
+++ controllers/project.js
@@ -1,7 +1,9 @@
 "use strict";
 
+const Schema = require("mongoose").Schema;
 const Project     = require("../models/project");
 const Task        = require("../models/task");
+const User        = require("../models/user");
 const error_types = require("./error_types");
 
 let controller = {
@@ -82,27 +84,61 @@ let controller = {
     },
 
     /**
-     * only can be updated the owned projects. Unless you are admin and can modify any project
+     * only can be updated the owned projects. Unless you are admin and can modify any project.
+     * First of all we check that new members aren't already project members.
+     * In case of adding or deleting members, we check that these exist.
      * Parameters via body:
      *  -name: String
      *  -color: String
+     *  -add_members: [ObjectId, ObjectId, ...]
+     *  -delete_members: [ObjectId, ObjectId, ...]     *
      */
     updateProject: (req, res, next) => {
         if(!req.params.id)
             next(new error_types.Error400("id param with project id is rquired."));
+        if(req.body.add_members && req.body.delete_members)
+            next(new error_types.Error400("It's not possible adding and deleting members in the same request."));
         let update = {};
         if(req.body.name) update["name"] = req.body.name;
         if(req.body.color) update["color"] = req.body.color;
-
-        Project.findById(req.params.id)
+        if(req.body.add_members) update["$push"] = { "members": { "$each" : req.body.add_members } };
+        if(req.body.delete_members) update["$pullAll"] = { "members": req.body.delete_members };
+        Project.findById(req.params.id).lean().exec()
             .then(data=>{
                 if(!data)
                     throw(new error_types.Error404("Project not found"));
                 else if (!data.owner.equals(req.user._id) && req.user.admin==false){
-                    throw(new error_types.Error403("You are not allowed to modify this project"));
+                    throw(new error_types.Error403("You are not allowed to modify this project."));
+                }
+                else if(req.body.add_members){
+                    data.members = data.members.map(e=>{
+                        e = e.toString();
+                        return e;
+                    });
+                    /*we make intersection between new users and existing users, it must be void.
+                    Otherwise it means a new member is already a member*/
+                    if(data.members.filter(e => req.body.add_members.includes(e)).length != 0)
+                        throw(new error_types.Error400("Some user you are trying to add is already a member."));
                 }
                 else
                     return Promise.resolve();
+            })
+            .then(()=>{
+                if(req.body.add_members){
+                    return User.countDocuments({_id: {"$in": req.body.add_members}})
+                        .then(count=>{
+                            if(count != req.body.add_members.length)
+                                throw new error_types.Error400("You are trying to add members that do not exist.");
+                        });
+                }
+                if(req.body.delete_members){
+                    return User.countDocuments({_id: {"$in": req.body.delete_members}})
+                        .then(count=>{
+                            if(count != req.body.delete_members.length)
+                                throw new error_types.Error400("You are trying to delete members that do not exist.");
+                        });
+                }
+                return Promise.resolve();
             })
             .then(()=>{
                 return Project.findByIdAndUpdate(req.params.id, update, {new:true});
