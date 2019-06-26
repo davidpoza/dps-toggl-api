cb2553b3ab2e065e3af80b5054620a1371c7dbe0
David Poza Suarez <pozasuarez@gmail.com>
2019-06-26 14:41:30 +0200

    added created_on field to user entity

--- models/user.js
+++ models/user.js
@@ -10,7 +10,13 @@ const UserSchema = Schema({
     last_name : String,
     admin     : Boolean,
     avatar    : String,
-    active    : Boolean
+    active    : Boolean,
+    created_on: Date
+});
+
+UserSchema.pre("save", function(next){
+    this.created_on = new Date();
+    return next();
 });
 
 module.exports = mongoose.model("User", UserSchema, "users");
