b2050e1e8e9aad14b0f1125c1e546e4d4e6f5f2a
David Poza Suárez <pozasuarez@gmail.com>
2019-09-16 18:28:08 +0200

    fix mongoose.connection.close

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 "use strict";
 require("dotenv").config();
-const mongoogse = require("mongoose");
+const mongoose = require("mongoose");
 
 const app       = require("./app");
 const logger = require("./utils/logger");
 const port      = process.env.PORT;
 
-mongoogse.Promise = global.Promise;
-mongoogse.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify:false })
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify:false })
     .then(() => {
         console.log("Success on mongoose connection");
         logger.log({message:"Success on mongoose connection", level:"info" });
